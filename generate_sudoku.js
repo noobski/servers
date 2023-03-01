c39ddd14eed028909076b023aebbb57367a68165
noobski <43997727+noobski@users.noreply.github.com>
2023-03-01 16:20:45 +0200

    lint

--- generate_sudoku.js
+++ generate_sudoku.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-magic-numbers */
 /* eslint-disable no-console */
-/* global require exports permutate_puzzle_entry */
+/* global require exports  */
 const puzzles_db = require('./sudoku_puzzles.js');
 
 exports.generate_new_board = generate_new_board;
@@ -9,6 +9,7 @@ const numbers_in_sudoku = [1,2,3,4,5,6,7,8,9];
 const empty_cell = 0;
 
 // test();
+// eslint-disable-next-line no-unused-vars
 function test(){
 	for (let i=0; i<1000; i++)
 	{
@@ -19,7 +20,6 @@ function test(){
 		}
 	}
 }
-
 // puzzle entry: {array:a, show:show, level:'easy'}
 function generate_new_board(level){
 	// choose a seed according to level
@@ -35,6 +35,7 @@ function generate_new_board(level){
 	// return a new entry
 	return entry;
 }
+// eslint-disable-next-line no-unused-vars
 function print_board(entry){
 	const a = entry.array, show = entry.show;
 	[a, show].forEach(array => {
@@ -51,7 +52,7 @@ function create_random_permutation(){
 	let permutation = new Array(9).fill(0);
 	let n=1;
 	const numbers=new Array(9).fill().map(() => n++);
-	permutation = permutation.map(e => {
+	permutation = permutation.map(() => {
 		const index = Math.floor(Math.random()*numbers.length);
 		const v = numbers[index];
 		numbers.splice(index,1);
@@ -74,77 +75,7 @@ function rotate_90(a){
 			b[c][8-r] = a[r][c];
 	return b;
 }
-
-
-function graph(level){
-	// graph level progress
-	let e = document.createElement('button');
-	e.style.width=level*7;
-	document.body.appendChild(e);
-	e = document.createElement('span');
-	e.innerHTML = level;
-	e.style.fontSize = 10;
-	document.body.appendChild(e);
-	document.body.appendChild(document.createElement('br'));
-}
-
-function test_creating_sudoku_board(){
-	// fill sudoku board with zeros
-	const a = Array(9).fill().map(()=>Array(9).fill(empty_cell));
-	sudoku_solve(1, a);
-
-}
-function sudoku_solve(level, a){
-	console.log(level++);
-	graph(level);
-	// check if finished (all cells are filled)
-	if(!a.flat().includes(empty_cell))
-		return true; // succeeded solving!
-	// choose random empty square
-	const [xr,xc] = find_random_empty_square();
-	const r = xr, c = xc;
-	// put a random (1-w) number in it that does not appear in its row and column
-	const candidates = find_eligible_numbers(r,c);
-	while(candidates.length)
-	{
-		a[r][c] = '*'; // debugging purposes
-		// choose a random candidate & remove it from the candidates list
-		const index = Math.floor(Math.random()*candidates.length);
-		const candidate = candidates[index];
-		candidates.splice(index,1);
-		a[r][c] = candidate;
-		// generate next sudoku number
-		const retval = sudoku_solve(level);
-		if(retval)
-			return true;
-		else
-			a[r][c] = empty_cell; // 'delete' candidate and choose next one (if exists)
-	}
-	// if no candidates at all, return failure (parent needs to change its candidate)
-	return false;
-}
-function find_eligible_numbers(r,c){
-	// get all the numbers from the 1/9th that this r,c is in
-	const existing = [];
-	let tl_r, tl_c;
-	tl_r = Math.floor(r/3)*3, tl_c = Math.floor(c/3)*3;
-	for(let i=tl_r; i<tl_r+3; i++)
-		for(let j=tl_c; j<tl_c+3; j++)
-			existing.push(a[i][j]);
-	// add all numbers from the row and column
-	const all_nums = existing.concat(a.map(x => x[c]).concat(a[r]));
-	// find all numbers from 1..w that don't exist in the existing numbers
-	return numbers_in_sudoku.filter(x => !all_nums.includes(x));
-}
-function find_random_empty_square(){
-	let r=-1, c;
-	while(r==-1 || (a[r][c]!=empty_cell))
-	{
-		r=Math.floor(Math.random()*9);
-		c=Math.floor(Math.random()*9);
-	}
-	return [r,c];
-}
+// eslint-disable-next-line no-unused-vars
 function validate_solution(entry){
 	const array = entry.array;
 	let row, col, ninth;
@@ -191,32 +122,4 @@ function validate_solution(entry){
 	}
 	console.log(entry.level, retval);
 	return retval;
-}
-function test_permutation(){
-	const a = [[4,2,7,8,6,5,9,1,3],
-		[9,1,5,2,4,3,6,8,7],
-		[6,8,3,7,9,1,2,5,4],
-		[8,7,1,6,2,9,3,4,5],
-		[3,4,9,1,5,8,7,2,6],
-		[2,5,6,3,7,4,8,9,1],
-		[5,9,8,4,3,7,1,6,2],
-		[1,3,2,5,8,6,4,7,9],
-		[7,6,4,9,1,2,5,3,8]];
-	const show =  [[0,0,0,1,0,1,0,1,1],
-		[0,0,0,1,0,1,1,0,0],
-		[1,0,0,0,1,0,1,0,1],
-		[0,0,0,0,0,0,0,0,1],
-		[0,1,0,1,0,0,1,0,1],
-		[1,1,1,1,0,1,1,1,0],
-		[1,1,0,0,0,1,1,0,1],
-		[1,0,1,0,1,0,1,1,0],
-		[0,0,1,1,1,0,0,1,1]];
-	const entry = {array:a, show:show, level:'easy'};
-
-	// test the permutation
-	permutate_puzzle_entry(entry);
-}
-function test_new_board_creation(){
-	sudoku_solve(1);
-	validate_solution(a);
 }
